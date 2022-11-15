e196f6a36fbc750ecc223ec7bd4ac4cda84285af
Renan Araújo <renan.araujoonline@gmail.com>
2022-11-15 20:37:03 -0300

    add new users via POST

--- app.js
+++ app.js
@@ -1,15 +1,59 @@
+const bcrypt = require('bcrypt');
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+const dbConnect = require('./db/dbConnect');
+const User = require('./db/userModel');
+
+//execute db connection
+dbConnect();
 
 // body parser configuration
 app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (request, response, next) => {
   response.json({ message: "Hey! This is your server response!" });
   next();
 });
 
+app.post('/register', (request, response) =>{
+  //hash password
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) =>{
+      //create new user instance + collect data
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      })
+
+      //save new user
+      // console.log(user)
+      user
+        .save()
+        //return success if new user is added to database
+        .then((result) => {
+          response.status(201).send({
+            message: "User created successfully",
+            result,
+          })
+        })
+        //catch error if new user wasnt added successfully
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error creating user",
+            error,
+          })
+        })
+    })
+    //catch error if password hash aint successful
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password hash wasnt successful",
+        e,
+      })
+    })
+})
 
 module.exports = app;
---
+++ db/dbConnect.js
@@ -0,0 +1,27 @@
+// imports
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+async function dbConnect(){
+  // using mongoose to connect to the database on mongoDB using DB_URL -- connection string
+  mongoose
+    .connect(
+      process.env.DB_URL,
+      {
+        //to ensure connection is done properly
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useCreateIndex: true,
+      }
+    )
+    //checking to see if connection was successful or not
+    .then(() => {
+      console.log(" you've got this! Connection ok!!");
+    })
+    .catch((error) => {
+      console.log('Couldnt connect to Atlas');
+      console.error(error)
+    })
+}
+
+module.exports = dbConnect;

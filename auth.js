f95d2a7a2b8a526a4149311f13c2f23ca0140d22
Renan Araújo <renan.araujoonline@gmail.com>
2022-11-17 10:06:02 -0300

    checking commit

--- auth.js
+++ auth.js
@@ -2,18 +2,18 @@ const jwt = require("jsonwebtoken");
 
 module.exports = async (request, response, next) => {
   try{
-    //get token from authorization header
+    // get token from authorization header
     const token = await request.headers.authorization.split(" ")[1];
-    //check if token matches the supposed origin
+    // check if token matches the supposed origin
     const decodedToken = await jwt.verify(
       token,
       "RANDOM-TOKEN"
     )
-    //retrieve user details of the logged in user
+    // retrieve user details of the logged in user
     const user = await decodedToken;
     //pass user down to the endpoints here
     request.user = user;
-    //pass down funcionality to the endpoint
+    // pass down funcionality to the endpoint
     next();
   } catch (error){
     response.status(401).json({
